3256437a5bc443de1e6ebdc4451a2b6b0d7f0d04
Sneta1 <45085960+Sneta1@users.noreply.github.com>
2018-11-15 20:44:26 -0600

    Add files via upload

---
+++ include.js
@@ -0,0 +1,9 @@
+
+ /* calling the footer and header*/
+ $(function(){
+      $("#footer").load("footer.html"); 
+	  $("#header").load("header.html"); 
+	  $("#slider").load("slider.html");
+    });
+
+//calling the header 
